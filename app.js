41da82d8ee31930b1d3ac2c171a380bae2a76cf7
yangchufa <hs001@hs.com>
2025-12-02 11:16:18 +0800

    增加路由

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const routes = require("./routes"); //新增
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use("/", routes); //新增
+
+//删除
+// app.get("/", (req, res) => {
+//   res.send("Hello World");
+// });
 
 app.listen(4000, () => {
   console.log("server is running");
